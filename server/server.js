f48c2f2cf44d91eb05a556f1f0eca12625a8889c
trieu <chautrieu@gmail.com>
2018-05-21 00:50:09 +0700

    init server

---
+++ server/server.js
@@ -0,0 +1,42 @@
+import http from "http";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost/mastering-full-stack-react-web-development");
+
+const articleSchema = {
+  articleTitle: String,
+  articleContent: String
+};
+
+const Article = mongoose.model("Article", articleSchema, "articles");
+
+const app = express();
+app.server = http.createServer(app);
+
+// CORS - 3rd party middleware
+app.use(cors());
+
+// This is required by falcor-express middleware
+// to work correctly with falcor-browser
+app.use(bodyParser.json({extended: false}));
+
+app.get("/", (req, res) => {
+  Article.find( (err, articlesDocs) => {
+    const ourArticles = articlesDocs.map( (articleItem) => {
+      return `<h2>${articleItem.articleTitle}</h2>
+        ${articleItem.articleContent}`
+    }).join("<br/>");
+
+    res.send(`<h1>Publish App Initial Application!</h1>
+      ${ourArticles} 
+    `)
+  });
+});
+
+app.server.listen(process.env.PORT || 3000);
+console.log(`Started on port ${app.server.address().port}`);
+
+export default app;
